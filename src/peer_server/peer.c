2f4d6dd30d1f54a64ccb0e7023ec9400ad1ebe95
Kevin James <KevinJames@thekev.in>
2016-06-06 19:15:50 -0400

    refactor peer.init and peer.join

--- src/peer_server/peer.c
+++ src/peer_server/peer.c
@@ -3,6 +3,7 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,12 +14,21 @@
 #include "../shared/address.h"
 #include "../shared/socket.h"
 
-int main(int argc, char *argv[]) {
-    if (argc != 1 && argc != 3) {
-        printf("usage: ./peer [<address> <port>]\n");
-        exit(1);
-    }
+void join(char *ip, uint8_t port, char *args[]) {
+    int8_t sockfd = -1;
+    connect_to_server(&sockfd, args[1], args[2]);
 
+    char command[strlen("newpeer:") + sizeof(ip) + 1 + sizeof(port) + 1];
+    sprintf(command, "newpeer:%s:%d", ip, port);
+    send_command(sockfd, command);
+
+    char buf[SOCKET_TRANSFER_LIMIT];
+    receive_response(sockfd, buf); // TODO: receives peer list
+
+    disconnect_from_server(sockfd);
+}
+
+void init(int8_t *sockfd, socklen_t *alen, bool join_network, char *args[]) {
     char *ip = select_ip();
 
     struct sockaddr_in server;
@@ -26,24 +36,23 @@ int main(int argc, char *argv[]) {
     inet_aton(ip, (struct in_addr *)&server.sin_addr.s_addr);
     server.sin_port = 0;
 
-    int8_t sockfd = -1;
-    socklen_t alen = 0;
-    create_server(&sockfd, &server, &alen);
+    create_server(sockfd, &server, alen);
     printf("%s %d\n", ip, server.sin_port);
 
-    if (argc == 3) {
-        int8_t peer_sockfd = -1;
-        connect_to_server(&peer_sockfd, argv[1], argv[2]);
-
-        char command[strlen("newpeer:") + sizeof(ip) + 1 + sizeof(server.sin_port) + 1];
-        sprintf(command, "newpeer:%s:%d", ip, server.sin_port);
-        send_command(peer_sockfd, command);
-
-        char buf[SOCKET_TRANSFER_LIMIT];
-        receive_response(peer_sockfd, buf);  // TODO: receives peer list
-
-        disconnect_from_server(peer_sockfd);
+    if (join_network) {
+        join(ip, server.sin_port, args);
     }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 1 && argc != 3) {
+        printf("usage: ./peer [<address> <port>]\n");
+        exit(1);
+    }
+
+    int8_t sockfd = -1;
+    socklen_t alen = 0;
+    init(&sockfd, &alen, argc == 3, argv);
 
     for (;;) {
         if (listen(sockfd, 0) < 0) {
