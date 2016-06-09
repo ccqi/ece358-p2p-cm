9701643db361df4e3a453d75ac8ced89406aaecf
Kevin James <KevinJames@thekev.in>
2016-06-09 16:27:34 -0400

    add content.cpp todo

--- src/peer_server/content.cpp
+++ src/peer_server/content.cpp
@@ -1,4 +1,5 @@
 #include <unordered_map>
+#include <stdlib.h>
 
 #include "content.h"
 
@@ -8,10 +9,18 @@ char *insert_content(char *value) {
     // TODO: find unused key
     char *key = (char *)"123";
     data[key] = value;
+    printf("%s\n", data.at(key));
     return key;
 }
 
 char *read_content(char *key) {
+    // TODO: why does this not have the value that line 12 has?
+    // Test case:
+    // $ ./peer
+    // $ ./addcontent peer_ip peer_port "test"
+    // test  # printed in ./peer window
+    // $ ./lookupcontent peer_ip peer_port 123
+    // # error in ./peer window
     return data.at(key);
 }
 
