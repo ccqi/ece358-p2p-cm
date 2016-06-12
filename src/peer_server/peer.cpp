97acfed093c83b2798f0177ba81a6f967c76ec01
Clarisse Schneider <cmschnei@uwaterloo.ca>
2016-06-11 23:17:18 -0400

    baby steps

--- src/peer_server/peer.cpp
+++ src/peer_server/peer.cpp
@@ -19,21 +19,26 @@
 
 struct addrInfo {
   char *ip;
-  uint8_t port;
+  char *port;
 
-  addrInfo(char *_ip, uint8_t _port) { ip = _ip; port = _port; }
+  addrInfo(char *_ip, char *_port) { ip = _ip; port = _port; }
   addrInfo() {}
 } selfInfo, left, right;
 
 int totalPeers;
 int totalContent;
 
-void addPeer(char *ip, uint8_t port) {
+void addPeer(char *ip, char *port) {
+  printf("%s:%s adding peer %s:%s\n", selfInfo.ip, selfInfo.port, ip, port);
+  printf("%s:%s right is currently %s:%s\n", selfInfo.ip, selfInfo.port, right.ip, right.port);
   if (right.ip == NULL) {
-    right = left = addrInfo(ip, port);
+    right = addrInfo(ip, port);
+    left = addrInfo(ip, port);
+    printf("%s:%s right is now %s:%s\n", selfInfo.ip, selfInfo.port, right.ip, right.port);
   } else {
     addrInfo oldRight = right;
     right = addrInfo(ip, port);
+    printf("%s:%s telling %s:%s to clone\n", selfInfo.ip, selfInfo.port, right.ip, right.port);
     int8_t rightSockfd = -1;
     connect_to_server(&rightSockfd, right.ip, right.port);
 
@@ -41,21 +46,18 @@ void addPeer(char *ip, uint8_t port) {
     ss << "clonepeer:" << totalPeers << ":" << totalContent << ":" << selfInfo.ip << ":" << selfInfo.port << ":" << oldRight.ip << ":" << oldRight.port;
     send_to_socket(rightSockfd, ss.str().c_str());
 
-    char buf[SOCKET_TRANSFER_LIMIT];
-    receive_from_socket(rightSockfd, buf); // TODO: receive success
-
     disconnect_from_server(rightSockfd);
   }
 }
 
-void clonePeer(int _totalPeers, int _totalContent, char *ipLeft, uint8_t portLeft, char *ipRight, uint8_t portRight) {
+void clonePeer(int _totalPeers, int _totalContent, char *ipLeft, char *portLeft, char *ipRight, char *portRight) {
     totalPeers = _totalPeers++;
     totalContent = _totalContent;
 
     left = addrInfo(ipLeft, portLeft);
     right = addrInfo(ipRight, portRight);
 
-    printf("created %d with left %d and right %d\n", selfInfo.port, left.port, right.port);
+    printf("created %s with left %s and right %s\n", selfInfo.port, left.port, right.port);
 
     int8_t rightSockfd = -1;
     connect_to_server(&rightSockfd, right.ip, right.port);
@@ -67,16 +69,16 @@ void clonePeer(int _totalPeers, int _totalContent, char *ipLeft, uint8_t portLef
     disconnect_from_server(rightSockfd);
 }
 
-void connectNewPeer(char *ipReplace, uint8_t portReplace, char *ipNew, uint8_t portNew) {
+void connectNewPeer(char *ipReplace, char *portReplace, char *ipNew, char *portNew) {
     if (selfInfo.ip == ipNew && selfInfo.port == portNew) {
       // completed full round of circle
       return;
     }
-    
+
     if (left.ip == ipReplace && left.port == portReplace) {
       // replace left with the new peer
       left = addrInfo(ipNew, portNew);
-      printf("%d left is now %d\n", selfInfo.port, left.port);
+      printf("%s left is now %s\n", selfInfo.port, left.port);
     }
     totalPeers++;
 
@@ -91,20 +93,16 @@ void connectNewPeer(char *ipReplace, uint8_t portReplace, char *ipNew, uint8_t p
     disconnect_from_server(rightSockfd);
 }
 
-void join(char *ip, uint8_t port, char *args[]) {
-    printf("%s:%d trying to join %s:%s\n", ip, port, args[1], args[2]);
+void join(char *ip, char *port, char *args[]) {
     int8_t sockfd = -1;
     connect_to_server(&sockfd, args[1], args[2]);
+    printf("%s:%s joined %s:%s\n", ip, port, args[1], args[2]);
 
     std::stringstream ss;
     ss << "newpeer:" << ip << ":" << port;
     send_to_socket(sockfd, ss.str().c_str());
 
-    char buf[SOCKET_TRANSFER_LIMIT];
-    receive_from_socket(sockfd, buf); // TODO: receives peer list
-
     disconnect_from_server(sockfd);
-    selfInfo = addrInfo(ip, port);
 }
 
 void init(int8_t *sockfd, socklen_t *alen, bool join_network, char *args[]) {
@@ -117,10 +115,12 @@ void init(int8_t *sockfd, socklen_t *alen, bool join_network, char *args[]) {
 
     create_server(sockfd, &server, alen);
     printf("%s %d\n", ip, ntohs(server.sin_port));
+    char *port = new char[6];
+    sprintf(port, "%d", server.sin_port);
+    selfInfo = addrInfo(ip, port);
 
     if (join_network) {
-    printf("join network\n");
-        join(ip, server.sin_port, args);
+        join(ip, port, args);
     }
 }
 
@@ -177,7 +177,7 @@ int main(int argc, char *argv[]) {
             // remove self from network
         } else if (strcmp(buf, "newpeer") == 0) {
             char *ip = strtok(NULL, ":");
-            uint8_t port = atoi(strtok(NULL, ":"));
+            char *port = strtok(NULL, ":");
             addPeer(ip, port);
             // add peer with ip and port to network
         } else if (strcmp(buf, "removepeer") == 0) {
@@ -190,22 +190,22 @@ int main(int argc, char *argv[]) {
             int totalPeers = atoi(strtok(NULL, ":"));
             int totalContent = atoi(strtok(NULL, ":"));
             char *ipLeft = strtok(NULL, ":");
-            uint8_t portLeft = atoi(strtok(NULL, ":"));
+            char *portLeft = strtok(NULL, ":");
             char *ipRight = strtok(NULL, ":");
-            uint8_t portRight = atoi(strtok(NULL, ":"));
+            char *portRight = strtok(NULL, ":");
             clonePeer(totalPeers, totalContent, ipLeft, portLeft, ipRight, portRight);
         } else if (strcmp(buf, "connectnewpeer") == 0) {
             char *ipReplace = strtok(NULL, ":");
-            uint8_t portReplace = atoi(strtok(NULL, ":"));
+            char *portReplace = strtok(NULL, ":");
             char *ipNew = strtok(NULL, ":");
-            uint8_t portNew = atoi(strtok(NULL, ":"));
+            char *portNew = strtok(NULL, ":");
             connectNewPeer(ipReplace, portReplace, ipNew, portNew);
         } else {
             // TODO: some sort of error
         }
         // TODO: Error cases
 
-        disconnect_from_server(connectedsock);
+        //disconnect_from_server(connectedsock);
 
         return 0;
     }
--- src/shared/socket.h
+++ src/shared/socket.h
@@ -6,13 +6,11 @@
 #define PORT_RANGE_LO 10000
 #define PORT_RANGE_HI 11000
 
-#define SOCKET_TRANSFER_LIMIT 16
+#define SOCKET_TRANSFER_LIMIT 32
 
 void create_server(int8_t *sockfd, void *server, socklen_t *alen);
 
 void connect_to_server(int8_t *sockfd, char *ip, char *port);
-
-void connect_to_server(int8_t *sockfd, char *ip, uint8_t port);
 
 void disconnect_from_server(uint8_t sockfd);
 
